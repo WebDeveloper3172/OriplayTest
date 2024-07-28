7963e7f5988a57335a95a989cf6efef9a741a542
chirilcristina <chirilcristina2002@gmail.com>
2024-07-28 08:46:18 +0300

    You can move the player with the stick

---
+++ OriplayTest/Assets/Script/Input/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+
+    private void OnEnable()
+    {
+        // Subscribe to the input system
+        var playerInput = new PlayerInput();
+        playerInput.Player.Move.performed += OnMove;
+        playerInput.Player.Move.canceled += OnMove;
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the input system
+        var playerInput = new PlayerInput();
+        playerInput.Player.Move.performed -= OnMove;
+        playerInput.Player.Move.canceled -= OnMove;
+        playerInput.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        // Apply the movement input to the player's position
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
+    }
+}
